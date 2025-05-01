d70cc7503dd73193a5077a02f99700b3d163f6e5
Samar Khaled <samar.khaled@gmail.com>
2025-05-01 13:27:35 +0300

    Add new styles for button and label

--- SwiftUI-Examples/ButtonsView/ButtonsView.swift
+++ SwiftUI-Examples/ButtonsView/ButtonsView.swift
@@ -32,10 +32,68 @@ struct ButtonsView: View {
             LinearGradientButton()
 
             Spacer()
+
+            VStack(spacing: 16) {
+                Label("Favorite", systemImage: "star")
+                    .labelStyle(AdaptiveLabelStyle())
+
+                Label("Favorite", systemImage: "star")
+                    .labelStyle(.adaptive)
+
+                Button("Start", systemImage: "play.circle") {}
+                    .buttonStyle(CapsuleButtonStyle(.orange))
+
+                Button("Start", systemImage: "play.circle") {}
+                    .buttonStyle(.capsule(.yellow))
+            }
         }
     }
 }
 
 #Preview {
     ButtonsView()
+}
+
+struct AdaptiveLabelStyle: LabelStyle {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+
+    func makeBody(configuration: Configuration) -> some View {
+        if horizontalSizeClass == .compact {
+            VStack {
+                configuration.icon
+                configuration.title
+            }
+        } else {
+            Label(configuration)
+        }
+    }
+}
+
+extension LabelStyle where Self == AdaptiveLabelStyle {
+    static var adaptive: AdaptiveLabelStyle {
+        AdaptiveLabelStyle()
+    }
+}
+
+struct CapsuleButtonStyle: ButtonStyle {
+    let color: Color
+
+    init(_ color: Color) {
+        self.color = color
+    }
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .foregroundStyle(.black)
+            .background(color, in: Capsule())
+            .fontDesign(.monospaced)
+            .symbolVariant(configuration.isPressed ? .fill : .none)
+    }
+}
+
+extension ButtonStyle where Self == CapsuleButtonStyle {
+    static func capsule(_ color: Color) -> CapsuleButtonStyle {
+        CapsuleButtonStyle(color)
+    }
 }
